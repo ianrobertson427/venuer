f3dd9bf1eee02246abe670aaccfe09e6fd58212d
“irobetson” <ianrobertson427@gmail.com>
2020-08-22 18:31:49 -0500

    Adding JSON web tokens

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   namespace :api do
     get "/sample_url" => "users#sample_action"
     post "/users" => "users#create"
+    post "/sessions" => "sessions#create"
   end
 end
